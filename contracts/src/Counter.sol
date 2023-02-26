81166b2a258171a7715c136da5a1f1bac56cc07d
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-26 05:00:04 -0800

    add attestation station contracts

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.15;
 
 contract Counter {
     uint256 public number = 0;
-    
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     function setNumber(uint256 newNumber) public {
