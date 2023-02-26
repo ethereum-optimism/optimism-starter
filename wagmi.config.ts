ee19c4b8fbd5e0f85ccc92790a91f0392fc01068
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-26 05:00:04 -0800

    add attestation station contracts

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@wagmi/cli'
 import { foundry, react } from '@wagmi/cli/plugins'
 import * as chains from 'wagmi/chains'
+import {ATTESTATION_STATION_ADDRESS} from '@eth-optimism/atst'
 
 export default defineConfig({
   out: 'src/generated.ts',
@@ -12,8 +13,12 @@ export default defineConfig({
           [chains.goerli.id]: '0x78991BB1D194C1235fe285240af8489CFA552151',
           [chains.foundry.id]: '0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167',
         },
+        AttestationStation: {
+          [chains.optimism.id]: ATTESTATION_STATION_ADDRESS,
+          [chains.optimismGoerli.id]: ATTESTATION_STATION_ADDRESS,
+          [chains.foundry.id]: ATTESTATION_STATION_ADDRESS,
+        }
       },
-      project: './contracts',
     }),
     react(),
   ],
