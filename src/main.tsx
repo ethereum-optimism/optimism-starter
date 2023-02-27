051473552a28ad9e797574ad0335f99bde9f6c8e
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-26 20:00:14 -0800

    Documentation
    se-2
    
    documentation
    
    add typescript FAQ
    
    Update README.md
    
    Add OP logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import '@rainbow-me/rainbowkit/styles.css'
+import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
@@ -7,12 +7,18 @@ import { WagmiConfig } from "wagmi";
 import { App } from "./App";
 import { chains, client } from "./wagmi";
 
+/**
+ * Root providers and initialization of app
+ * @see https://reactjs.org/docs/strict-mode.html 
+ * @see https://wagmi.sh/react/WagmiConfig 
+ * @see https://www.rainbowkit.com/docs/installation 
+ */
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<WagmiConfig client={client}>
-			<RainbowKitProvider chains={chains}>
-				<App />
-			</RainbowKitProvider>
-		</WagmiConfig>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
 );
