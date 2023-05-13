614e7f51e4c4b0bac77318b13f9d0168839157a0
Owklama <owklamaa@gmail.com>
2023-05-13 16:05:14 +0100

    chore: added ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-
 import { useAccount } from "wagmi";
 import Header from "./components/Header/Header";
 import RouterConfig from "./layout/RouterConfig";
 import { Outlet } from "react-router-dom";
 
-
 export function App() {
-
   return (
     <div className="bg-primary h-screen p-5">
-    <Header/>
-    <h1>Welcome to Minty Badget!</h1>
+      <Header />
+      <h1>Welcome to Minty Badget!</h1>
 
-    <main className="main-wrapper">
+      <main className="main-wrapper">
         <RouterConfig />
         <Outlet />
-        {/* {checkDisclaimer && (
-          <Disclaimer
-            setIsOpen={setIsOpen}
-            isOpen={isOpen}
-            handleAccepted={handleAccepted}
-          />
-        )} */}
       </main>
     </div>
   );
